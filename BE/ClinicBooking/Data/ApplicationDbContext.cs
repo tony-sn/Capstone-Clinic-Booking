161adb7bb467477504907922b519bac07d1910a0
Tony Nguyen <57897153+tony-sn@users.noreply.github.com>
2025-06-08 10:33:21 +1000

    chore: merge conflict

--- BE/ClinicBooking/Data/ApplicationDbContext.cs
+++ BE/ClinicBooking/Data/ApplicationDbContext.cs
@@ -1,22 +1,40 @@
-﻿using ClinicBooking.Models;
+using ClinicBooking.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace ClinicBooking.Data
 {
-    public class ApplicationDbContext : IdentityDbContext<User, Role, int, IdentityUserClaim<int>,
-        UserRole, IdentityUserLogin<int>, IdentityRoleClaim<int>, IdentityUserToken<int>>
+    public class ApplicationDbContext
+        : IdentityDbContext<
+            User,
+            Role,
+            int,
+            IdentityUserClaim<int>,
+            UserRole,
+            IdentityUserLogin<int>,
+            IdentityRoleClaim<int>,
+            IdentityUserToken<int>
+        >
     {
         private readonly IHttpContextAccessor _httpContextAccessor;
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, IHttpContextAccessor httpContextAccessor) : base(options)
+
+        public ApplicationDbContext(
+            DbContextOptions<ApplicationDbContext> options,
+            IHttpContextAccessor httpContextAccessor
+        )
+            : base(options)
         {
             _httpContextAccessor = httpContextAccessor;
         }
 
         public DbSet<Appointment> Appointments { get; set; }
+        public DbSet<RefreshToken> RefreshTokens { get; set; }
 
-        public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default)
+        public override Task<int> SaveChangesAsync(
+            bool acceptAllChangesOnSuccess,
+            CancellationToken cancellationToken = default
+        )
         {
             var userName = _httpContextAccessor.HttpContext?.User?.Identity?.Name ?? "System";
             foreach (var entry in ChangeTracker.Entries<EntityBase>())
