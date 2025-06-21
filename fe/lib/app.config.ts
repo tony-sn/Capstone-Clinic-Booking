41ce6f0dc61d0db513b13ff5b5e73af31089b29f
Tony Nguyen <57897153+tony-sn@users.noreply.github.com>
2025-06-21 13:13:34 +1000

    wip: wrap frontend with provider

---
+++ fe/lib/app.config.ts
@@ -0,0 +1,14 @@
+export const {
+  NEXT_PUBLIC_ENDPOINT: ENDPOINT,
+  PROJECT_ID,
+  API_KEY,
+  DATABASE_ID,
+  PATIENT_COLLECTION_ID,
+  DOCTOR_COLLECTION_ID,
+  APPOINTMENT_COLLECTION_ID,
+  NEXT_PUBLIC_BUCKET_ID: BUCKET_ID,
+} = process.env;
+
+export const Endpoints = {
+  APPOINTMENTS: `${ENDPOINT}/api/appointments`,
+};
