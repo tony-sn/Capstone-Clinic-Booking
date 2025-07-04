5fbc0b9fa662e73abcafd83612cdc981772a0f56
Le Huu Khuong <ilehuukhuong@gmail.com>
2025-07-04 17:31:13 +0700

    feat(medical-history): calculate TotalAmount from related data

--- BE/ClinicBooking/Services/IServices/IMedicalHistoryService.cs
+++ BE/ClinicBooking/Services/IServices/IMedicalHistoryService.cs
@@ -1,5 +1,5 @@
-using ClinicBooking_Utility;
 using ClinicBooking.Models.DTOs;
+using ClinicBooking_Utility;
 
 namespace ClinicBooking.Services.IServices;
 
@@ -10,5 +10,5 @@ public interface IMedicalHistoryService
     Task<MedicalHistoryDTO> Create(MedicalHistoryRequest request);
     Task<MedicalHistoryDTO> Update(int id, MedicalHistoryRequest request);
     Task<MedicalHistoryDTO> DeleteById(int id);
-
+    Task<MedicalHistoryDTO> CalculateTotalAmount(int medicalHistoryId);
 }
