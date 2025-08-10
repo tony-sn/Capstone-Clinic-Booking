593578906bf467ce4df3c689054e7e5ef8fc3674
Tony Nguyen <57897153+tony-sn@users.noreply.github.com>
2025-08-10 19:52:52 +1000

    style(ui): change layout of admin or appointments

--- fe/components/layout/StaffLayout.tsx
+++ fe/components/layout/StaffLayout.tsx
@@ -10,7 +10,9 @@ const StaffLayout = ({ userInfo, children }: StaffLayoutProps) => {
   return (
     <div className="mx-auto flex max-w-7xl flex-col space-y-14">
       <Navbar isAuthed userInfo={userInfo} />
-      <main className="admin-main">{children}</main>
+      <div className="mx-3 min-h-screen w-full rounded-2xl bg-gradient-to-br from-blue-50 to-white">
+        <main className="admin-main py-6">{children}</main>
+      </div>
     </div>
   );
 };
